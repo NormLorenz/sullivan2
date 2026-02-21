337c57ba632a8b8b85d750f1f3b027e5f665e297
NormLorenz <normlorenz@gmail.com>
2026-02-21 09:39:21 -0800

    feat: Display VITE_MY_SECRET in development
    Adds a conditional banner to the App component that displays the value of VITE_MY_SECRET. This is intended for development and debugging purposes. The .env.example file has been updated to include the new variable.

--- App.tsx
+++ App.tsx
@@ -13,6 +13,11 @@ const App: React.FC = () => {
   return (
     <Router>
       <div className="min-h-screen flex flex-col font-sans">
+        {import.meta.env.VITE_MY_SECRET && (
+          <div className="bg-blue-200 text-center p-2 font-bold">
+            The secret is: {import.meta.env.VITE_MY_SECRET}
+          </div>
+        )}
         <Navbar />
         <main className="flex-grow">
           <Routes>
